eef4d4e14f58dbc398941fce471fc9300387a526
yymmt <y.ymmt1984@gmail.com>
2023-02-11 22:27:13 +0900

    目次を追加

--- text-concat.user.js
+++ text-concat.user.js
@@ -83,11 +83,69 @@
   font-size: 12px;
   line-height: 1;
 }
+
+.indexAll {
+    position: fixed;
+    right: -4px;
+    top: 7px;
+    min-width: 100px;
+    max-width: 400px;
+    z-index: 999;
+}
+
+.indexAll>*>* {
+    display: none;
+}
+.indexAll.__open .__open {
+    display: block;
+}
+.indexAll.__close .__close {
+    display: block;
+}
+.button__open,
+.button__close {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    width: 32px;
+    height: 32px;
+    cursor: pointer;
+}
+.indexInner {}
+.index__open {
+    max-height: 80vh;
+    overflow-y: scroll;
+}
+.indexAll a {
+    display: inline-block;
+    padding: 5px 0px;
+}
 </style>
             `;
             $(win.document).find(".note-index").remove();
             $(win.document).find(".col-md-3, .col-md-push-9, .col-md-9, .col-md-pull-3").removeClass("col-md-3 col-md-push-9 col-md-9 col-md-pull-3");
             (async () => {
+                let indexAll=win.document.createElement("div");
+                indexAll.classList.add("indexAll");
+                indexAll.classList.add("__close");
+                indexAll.innerHTML=`
+                    <div class="indexInner">
+                        <pre class="index__open __open"></pre>
+                        <span class="button__close __open __toggle">✖️</span>
+                        <pre class="index__close __close">目次</pre>
+                        <span class="button__open __close __toggle">▼</span>
+                    </div>
+                `;
+                let indexMain=indexAll.querySelector(".index__open");
+                let addIndex = html => {
+                    let line=win.document.createElement("div");
+                    line.innerHTML=html;
+                    indexMain.appendChild(line);
+                };
+                let indexTextNodes = e => Array.from(e.childNodes).filter(n=>n.nodeType==1 || n.nodeType==3).map(n=> n.nodeType==1?indexTextNodes(n) : n.textContent).flat();
+                $(indexAll).find(".__toggle").on("click", e=> $(indexAll).toggleClass("__open").toggleClass("__close") );
+                win.document.body.appendChild(indexAll);
+
                 for(let i=0;i<urls.length;i++) {
                     let url = urls[i];
                     await fetch(url)
@@ -100,11 +158,37 @@
                             doc.querySelectorAll("video").forEach((elm) => {
                                 elm.remove();
                             });
-                            doc.querySelectorAll(".curriculum-container>section").forEach((elm) => {
-                                con.appendChild(elm);
+                            let con2=win.document.createElement("div");
+                            con2.id=`lesson${i}`;
+                            addIndex(`Lesson ${i}`);
+                            doc.querySelectorAll(".curriculum-container>section").forEach((chapterElm) => {
+                                chapterElm.querySelectorAll("h2.index, h3.index").forEach(indexElm=>{
+                                    if(indexElm.tagName.toLowerCase()=="h2"){
+                                        chapterElm.id=`lesson${i}_${chapterElm.id.replace("-","")}`;
+                                        let tx=indexTextNodes(indexElm);
+                                        if(tx[0].includes("Lesson")){
+                                            // Lesson 0/Chapter 1/はじめに
+                                            addIndex(`  <a href="#${tx[0].toLowerCase().replace(/ /,"")}_${tx[1].toLowerCase().replace(/ /,"")}">${tx[1].replace(/[^0-9]/g,"")} ${tx[2]}</a>`);
+                                        }
+                                    }
+                                    else {
+                                        indexElm.parentElement.id=`lesson${i}_${indexElm.id.replace(/-/,"").replace(/-/,".")}`;
+                                        let tx=indexTextNodes(indexElm);
+                                        if(tx[0].includes("Lesson")){
+                                            // Lesson 0/Chapter 2/.1/本コースの概要
+                                            addIndex(`    <a href="#${tx[0].toLowerCase().replace(/ /,"")}_${tx[1].toLowerCase().replace(/ /,"")}${tx[2]}">${tx[1].replace(/[^0-9]/g,"")}${tx[2]} ${tx[3]}</a>`);
+                                        }
+                                        if(tx[0].includes("課題")){
+                                            // 課題/HTMLで文書を書いてみよう
+                                            addIndex(`    <a href="#${indexElm.parentElement.id}">${tx[0]} ${tx[1]}</a>`);
+                                        }
+                                    }
+                                });
+                                con2.appendChild(chapterElm);
                             });
+                            con.appendChild(con2);
                         });
-                    win.document.title = `${i} / ${urls.length}`;
+                    win.document.title = `${i+1} / ${urls.length}`;
                 }
                 win.document.title = location.href.match(/curriculums\/([^\/]+)/)[1];
 
