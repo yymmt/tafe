8a395347e2415ec1679a038759f266a2be3fcf80
yymmt <y.ymmt1984@gmail.com>
2023-02-12 09:27:33 +0900

    目次を追加

--- text-concat.user.js
+++ text-concat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         テキスト結合
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://techacademy.jp/mentor/courses/*/curriculums/*/lessons
