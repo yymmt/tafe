e6101df87db206e58699f1e999596be90f1f1401
Shige <16932213+ShigeUe@users.noreply.github.com>
2023-03-24 23:41:04 +0900

    リンクを判別するためのセレクタを追加
    コメントにGoogleドライブのリンクがあると誤認するので、そのためのセレクタを追加

--- diff-checker.user.js
+++ diff-checker.user.js
@@ -18,7 +18,7 @@
     let ism = (pat) => location.href.match(pat);
     if (ism(/mentor\/users\/\d+(#.*)?$/) || ism(/mentor(.training)?.reports/)) {
         (async ()=>{
-            let g=$("a[href*='drive.google.com']").attr("href").match(/folders.([a-zA-Z0-9_-]+)/)[1];
+            let g=$("a[href*='drive.google.com:has(i)']").attr("href").match(/folders.([a-zA-Z0-9_-]+)/)[1];
             if(g){
                 let kadaiToFolder = {
                     'kadai-html-1':'kadai-html',
